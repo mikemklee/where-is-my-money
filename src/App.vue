ce4eea7339cddbffac65e547c399aee96655f0b8
Mike Lee <mikemunkyu.lee@gmail.com>
2022-07-23 09:59:51 -0400

    Add PrimeVue and scaffold simple example

--- src/App.vue
+++ src/App.vue
@@ -4,19 +4,35 @@
 
 // Initial scaffold example
 // import HelloWorld from './components/HelloWorld.vue'
+const cars = [
+  { brand: "Volkswagen", year: 2012, color: "Orange", vin: "dsad231ff" },
+  { brand: "Audi", year: 2011, color: "Black", vin: "gwregre345" },
+  { brand: "Renault", year: 2005, color: "Gray", vin: "h354htr" },
+  { brand: "BMW", year: 2003, color: "Blue", vin: "j6w54qgh" },
+  { brand: "Mercedes", year: 1995, color: "Orange", vin: "hrtwy34" },
+  { brand: "Volvo", year: 2005, color: "Black", vin: "jejtyj" },
+  { brand: "Honda", year: 2012, color: "Yellow", vin: "g43gr" },
+  { brand: "Jaguar", year: 2013, color: "Orange", vin: "greg34" },
+  { brand: "Ford", year: 2000, color: "Black", vin: "h54hw5" },
+  { brand: "Fiat", year: 2013, color: "Red", vin: "245t2s" },
+];
+const columns = [
+  { field: "vin", header: "Vin" },
+  { field: "year", header: "Year" },
+  { field: "brand", header: "Brand" },
+  { field: "color", header: "Color" },
+];
 </script>
 
 <template>
-  <!-- Initial scaffold example -->
-  <!-- <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" /> -->
+  <DataTable :value="cars">
+    <Column
+      v-for="col of columns"
+      :field="col.field"
+      :header="col.header"
+      :key="col.field"
+    ></Column>
+  </DataTable>
 </template>
 
 <style scoped>
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,30 @@
 import { createApp } from 'vue'
-import './style.css'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+// PrimeVue imports
+import PrimeVue from 'primevue/config';
+import Dialog from 'primevue/dialog';
+import Button from 'primevue/button';
+import DataTable from 'primevue/datatable';
+import Column from 'primevue/column';
+import ColumnGroup from 'primevue/columngroup';     //optional for column grouping
+import Row from 'primevue/row';                     //optional for row
+
+
+import './style.css'
+import 'primevue/resources/themes/lara-light-indigo/theme.css';
+import 'primevue/resources/primevue.min.css';
+import 'primeicons/primeicons.css';
+
+const app = createApp(App)
+
+// PrimeVue configs
+app.use(PrimeVue)
+app.component('Dialog', Dialog);
+app.component('Button', Button);
+app.component('DataTable', DataTable);
+app.component('Column', Column);
+app.component('ColumnGroup', ColumnGroup);
+app.component('Row', Row);
+
+app.mount('#app')
