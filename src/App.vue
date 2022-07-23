ad2f6b761c236b9d8aa5e35e39d269d33307c1be
Mike Lee <mikemunkyu.lee@gmail.com>
2022-07-23 11:20:50 -0400

    Define expense tracker columns

--- src/App.vue
+++ src/App.vue
@@ -1,62 +1,80 @@
 <script setup lang="ts">
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
-
-// Initial scaffold example
-// import HelloWorld from './components/HelloWorld.vue'
-const cars = [
-  { brand: "Volkswagen", year: 2012, color: "Orange", vin: "dsad231ff" },
-  { brand: "Audi", year: 2011, color: "Black", vin: "gwregre345" },
-  { brand: "Renault", year: 2005, color: "Gray", vin: "h354htr" },
-  { brand: "BMW", year: 2003, color: "Blue", vin: "j6w54qgh" },
-  { brand: "Mercedes", year: 1995, color: "Orange", vin: "hrtwy34" },
-  { brand: "Volvo", year: 2005, color: "Black", vin: "jejtyj" },
-  { brand: "Honda", year: 2012, color: "Yellow", vin: "g43gr" },
-  { brand: "Jaguar", year: 2013, color: "Orange", vin: "greg34" },
-  { brand: "Ford", year: 2000, color: "Black", vin: "h54hw5" },
-  { brand: "Fiat", year: 2013, color: "Red", vin: "245t2s" },
+const items = [
+  {
+    date: 1658588456123,
+    source: "Longos",
+    title: "닭 허벅지살 + 요거트 + 시리얼",
+    income: 0,
+    expense: 18.29,
+    category: "변동지출",
+    "sub-category": "음식",
+    notes: "닭 600g, 요거트 500g, 시리얼 300g",
+  },
+  {
+    date: 1658588456123,
+    source: "Uber",
+    title: "핀치 - 집 택시비",
+    income: 0,
+    expense: 16.79,
+    category: "변동지출",
+    "sub-category": "교통",
+    notes: "",
+  },
+  {
+    date: 1658588456123,
+    source: "E-Transfer",
+    title: "동기모임 카티지여행 정산",
+    income: 0,
+    expense: 124.0,
+    category: "비정기지출",
+    "sub-category": "여가",
+    notes: "",
+  },
 ];
 const columns = [
-  { field: "vin", header: "Vin" },
-  { field: "year", header: "Year" },
-  { field: "brand", header: "Brand" },
-  { field: "color", header: "Color" },
+  { field: "date", header: "Date" },
+  { field: "source", header: "Source" },
+  { field: "title", header: "Title" },
+  { field: "income", header: "Income" },
+  { field: "expense", header: "Expense" },
+  { field: "category", header: "Category" },
+  { field: "sub-category", header: "Sub-category" },
+  { field: "notes", header: "Notes" },
 ];
 </script>
 
 <template>
-  <Card>
-    <template #title> New item </template>
-    <template #content>
-      <Button>Add</Button>
-    </template>
-  </Card>
-  <Card>
-    <template #title> Data </template>
-    <template #content>
-      <DataTable :value="cars">
-        <Column
-          v-for="col of columns"
-          :field="col.field"
-          :header="col.header"
-          :key="col.field"
-        />
-      </DataTable>
-    </template>
-  </Card>
+  <div class="p-12">
+    <Card class="mb-6">
+      <template #title><h1 class="font-semibold">New item</h1></template>
+      <template #content>
+        <Button>Add</Button>
+      </template>
+    </Card>
+    <Card>
+      <template #title>
+        <h1 class="font-semibold">History</h1>
+      </template>
+      <template #content>
+        <DataTable
+          :value="items"
+          showGridlines
+          resizableColumns
+          columnResizeMode="fit"
+          responsiveLayout="scroll"
+          class="p-datatable-sm"
+        >
+          <Column
+            v-for="col of columns"
+            :field="col.field"
+            :header="col.header"
+            :key="col.field"
+            class="text-sm"
+          />
+        </DataTable>
+      </template>
+    </Card>
+  </div>
 </template>
 
-<style scoped>
-/* Initial scaffold example */
-/* .logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-} */
-</style>
+<style scoped></style>
