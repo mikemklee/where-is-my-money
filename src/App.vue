cfbea96bf60b5b86d2c2806d2947b71d06ce30f1
Mike Lee <mikemunkyu.lee@gmail.com>
2022-07-23 15:54:33 -0400

    Add app header

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import { ref } from "vue";
+import Header from "./components/Header.vue";
 const items = [
   {
     date: 1658588456123,
@@ -59,112 +60,116 @@ const subCategoryValueRef = ref();
 </script>
 
 <template>
-  <div class="p-12 flex">
-    <Card class="mr-6 min-w-fit">
-      <template #title><h1 class="font-semibold">New item</h1></template>
-      <template #content>
-        <div class="flex flex-col">
-          <div class="grid grid-cols-2 gap-2 mb-4">
-            <!-- Date -->
-            <div class="flex flex-col">
-              <label for="basic">Date</label>
-              <Calendar id="basic" v-model="dateValRef" autocomplete="off" />
-            </div>
-            <!-- Amount -->
-            <div class="flex flex-col">
-              <label for="currency-us">Amount</label>
-              <InputNumber
-                id="currency-us"
-                v-model="amountValueRef"
-                mode="currency"
-                currency="USD"
-                locale="en-US"
-              />
-            </div>
-          </div>
-          <div class="flex flex-col mb-4">
-            <!-- Source -->
-            <div class="flex flex-col">
-              <h5>Source</h5>
-              <span class="p-float-label">
-                <InputText
-                  id="source"
-                  type="text"
-                  v-model="sourceValueRef"
-                  class="w-full"
-                />
-              </span>
-              <small id="source-help">e.g. Uber</small>
-            </div>
-            <!-- Title -->
-            <div class="flex flex-col">
-              <h5>Title</h5>
-              <span class="p-float-label">
-                <InputText
-                  id="title"
-                  type="text"
-                  v-model="titleValueRef"
-                  class="w-full"
-                />
-              </span>
-              <small id="title-help">e.g. trip from Downtown to home</small>
-            </div>
-          </div>
-          <div class="grid grid-cols-2 gap-2 mb-4">
-            <!-- Category -->
-            <div class="flex flex-col">
-              <h5>Category</h5>
-              <Dropdown
-                v-model="categoryValueRef"
-                :options="categoryOptions"
-                optionLabel="label"
-                optionValue="value"
-                placeholder="Select a category"
-              />
-            </div>
-            <!-- Sub-category -->
-            <div class="flex flex-col">
-              <h5>Sub-category</h5>
-              <Dropdown
-                v-model="subCategoryValueRef"
-                :options="subCategoryOptions"
-                optionLabel="label"
-                optionValue="value"
-                placeholder="Select a sub-category"
-              />
-            </div>
-          </div>
-        </div>
+  <div class="p-24 flex flex-col w-full h-full">
+    <Header />
 
-        <div class="flex justify-end">
-          <Button label="Add" class="p-button-sm" />
-        </div>
-      </template>
-    </Card>
-    <Card class="grow">
-      <template #title>
-        <h1 class="font-semibold">History</h1>
-      </template>
-      <template #content>
-        <DataTable
-          :value="items"
-          showGridlines
-          resizableColumns
-          autoLayout
-          columnResizeMode="fit"
-          responsiveLayout="scroll"
-          class="p-datatable-sm"
-        >
-          <Column
-            v-for="col of columns"
-            :field="col.field"
-            :header="col.header"
-            :key="col.field"
-            class="text-sm"
-          />
-        </DataTable>
-      </template>
-    </Card>
+    <div class="flex">
+      <Card class="mr-6 min-w-fit">
+        <template #title><h1 class="font-semibold">New item</h1></template>
+        <template #content>
+          <div class="flex flex-col">
+            <div class="grid grid-cols-2 gap-2 mb-4">
+              <!-- Date -->
+              <div class="flex flex-col">
+                <label for="basic">Date</label>
+                <Calendar id="basic" v-model="dateValRef" autocomplete="off" />
+              </div>
+              <!-- Amount -->
+              <div class="flex flex-col">
+                <label for="currency-us">Amount</label>
+                <InputNumber
+                  id="currency-us"
+                  v-model="amountValueRef"
+                  mode="currency"
+                  currency="USD"
+                  locale="en-US"
+                />
+              </div>
+            </div>
+            <div class="flex flex-col mb-4">
+              <!-- Source -->
+              <div class="flex flex-col">
+                <h5>Source</h5>
+                <span class="p-float-label">
+                  <InputText
+                    id="source"
+                    type="text"
+                    v-model="sourceValueRef"
+                    class="w-full"
+                  />
+                </span>
+                <small id="source-help">e.g. Uber</small>
+              </div>
+              <!-- Title -->
+              <div class="flex flex-col">
+                <h5>Title</h5>
+                <span class="p-float-label">
+                  <InputText
+                    id="title"
+                    type="text"
+                    v-model="titleValueRef"
+                    class="w-full"
+                  />
+                </span>
+                <small id="title-help">e.g. trip from Downtown to home</small>
+              </div>
+            </div>
+            <div class="grid grid-cols-2 gap-2 mb-4">
+              <!-- Category -->
+              <div class="flex flex-col">
+                <h5>Category</h5>
+                <Dropdown
+                  v-model="categoryValueRef"
+                  :options="categoryOptions"
+                  optionLabel="label"
+                  optionValue="value"
+                  placeholder="Select a category"
+                />
+              </div>
+              <!-- Sub-category -->
+              <div class="flex flex-col">
+                <h5>Sub-category</h5>
+                <Dropdown
+                  v-model="subCategoryValueRef"
+                  :options="subCategoryOptions"
+                  optionLabel="label"
+                  optionValue="value"
+                  placeholder="Select a sub-category"
+                />
+              </div>
+            </div>
+          </div>
+
+          <div class="flex justify-end">
+            <Button label="Add" class="p-button-sm" />
+          </div>
+        </template>
+      </Card>
+      <Card class="grow">
+        <template #title>
+          <h1 class="font-semibold">History</h1>
+        </template>
+        <template #content>
+          <DataTable
+            :value="items"
+            showGridlines
+            resizableColumns
+            autoLayout
+            columnResizeMode="fit"
+            responsiveLayout="scroll"
+            class="p-datatable-sm"
+          >
+            <Column
+              v-for="col of columns"
+              :field="col.field"
+              :header="col.header"
+              :key="col.field"
+              class="text-sm"
+            />
+          </DataTable>
+        </template>
+      </Card>
+    </div>
   </div>
 </template>
 
---
+++ src/components/Header.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="mb-8 ml-2">
+    <h1 class="italic text-5xl">WIMM</h1>
+    <h1 class="italic font-light text-xl">Where is my money?</h1>
+  </div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped></style>
