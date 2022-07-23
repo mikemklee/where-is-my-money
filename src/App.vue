dba13166ea96ea0c3b74555f306c2438e3242466
Mike Lee <mikemunkyu.lee@gmail.com>
2022-07-23 09:17:38 -0400

    Comment out scaffold

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,14 @@
 <script setup lang="ts">
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
-import HelloWorld from './components/HelloWorld.vue'
+
+// Initial scaffold example
+// import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
-  <div>
+  <!-- Initial scaffold example -->
+  <!-- <div>
     <a href="https://vitejs.dev" target="_blank">
       <img src="/vite.svg" class="logo" alt="Vite logo" />
     </a>
@@ -13,11 +16,12 @@ import HelloWorld from './components/HelloWorld.vue'
       <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
     </a>
   </div>
-  <HelloWorld msg="Vite + Vue" />
+  <HelloWorld msg="Vite + Vue" /> -->
 </template>
 
 <style scoped>
-.logo {
+/* Initial scaffold example */
+/* .logo {
   height: 6em;
   padding: 1.5em;
   will-change: filter;
@@ -27,5 +31,5 @@ import HelloWorld from './components/HelloWorld.vue'
 }
 .logo.vue:hover {
   filter: drop-shadow(0 0 2em #42b883aa);
-}
+} */
 </style>
