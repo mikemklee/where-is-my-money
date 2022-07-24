c2134c5547793cbefcdfc8bf48370463e438518c
Mike Lee <mikemunkyu.lee@gmail.com>
2022-07-24 12:51:04 -0400

    Extract SubCategoryField

--- src/components/EntryEditor/EntryEditor.vue
+++ src/components/EntryEditor/EntryEditor.vue
@@ -23,16 +23,8 @@
           <div class="flex flex-col">
             <CategoryField :category="categoryValueRef" />
           </div>
-          <!-- Sub-category -->
           <div class="flex flex-col">
-            <h5>Sub-category</h5>
-            <Dropdown
-              v-model="subCategoryValueRef"
-              :options="subCategoryOptions"
-              optionLabel="label"
-              optionValue="value"
-              placeholder="Select one"
-            />
+            <SubCategoryField :subCategory="subCategoryValueRef" />
           </div>
         </div>
       </div>
@@ -50,20 +42,15 @@ import AmountField from "./Fields/AmountField.vue";
 import CategoryField from "./Fields/CategoryField.vue";
 import DateField from "./Fields/DateField.vue";
 import SourceField from "./Fields/SourceField.vue";
+import SubCategoryField from "./Fields/SubCategoryField.vue";
 import TitleField from "./Fields/TitleField.vue";
-
-const subCategoryOptions = ref([
-  { label: "음식", value: "음식" },
-  { label: "교통", value: "교통" },
-  { label: "여가", value: "여가" },
-]);
 
 const dateValRef = ref(new Date());
 const sourceValueRef = ref("");
 const titleValueRef = ref("");
 const amountValueRef = ref(0);
 const categoryValueRef = ref(null);
-const subCategoryValueRef = ref();
+const subCategoryValueRef = ref(null);
 </script>
 
 <style scoped></style>
---
+++ src/components/EntryEditor/Fields/SubCategoryField.vue
@@ -0,0 +1,26 @@
+<template>
+  <h5>Sub-category</h5>
+  <Dropdown
+    v-model="subCategory"
+    :options="subCategoryOptions"
+    optionLabel="label"
+    optionValue="value"
+    placeholder="Select one"
+  />
+</template>
+
+<script setup lang="ts">
+import { ref } from "vue";
+
+defineProps<{
+  subCategory: string | null;
+}>();
+
+const subCategoryOptions = ref([
+  { label: "음식", value: "음식" },
+  { label: "교통", value: "교통" },
+  { label: "여가", value: "여가" },
+]);
+</script>
+
+<style scoped></style>
