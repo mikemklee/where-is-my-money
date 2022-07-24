cdae765fdca146765c8ac5fb81be95b0d99f821f
Mike Lee <mikemunkyu.lee@gmail.com>
2022-07-24 12:41:33 -0400

    Misc cleanup

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -9,17 +9,6 @@
   </div>
 </template>
 
-<script setup lang="ts">
-import { ref } from "vue";
-import { startOfMonth, endOfMonth } from "date-fns";
-
-const today = new Date();
-const firstDayOfMonth = startOfMonth(today);
-const lastDayOfMonth = endOfMonth(today);
-
-const rangeValRef = ref([firstDayOfMonth, lastDayOfMonth]);
-
-// TODO: persist this in store, so it can be used as a global filter
-</script>
+<script setup lang="ts"></script>
 
 <style scoped></style>
