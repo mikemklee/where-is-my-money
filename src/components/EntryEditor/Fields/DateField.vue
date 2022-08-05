ef93852a98812bf16991e0820f7dd1d3cebb23be
Mike Lee <mikemunkyu.lee@gmail.com>
2022-08-04 21:35:00 -0400

    Persist input value for Date field

--- src/components/EntryEditor/Fields/DateField.vue
+++ src/components/EntryEditor/Fields/DateField.vue
@@ -1,10 +1,23 @@
 <template>
   <label for="basic">Date</label>
-  <Calendar id="basic" :value="date" autocomplete="off" />
+  <Calendar
+    id="basic"
+    :modelValue="date"
+    autocomplete="off"
+    :showTime="false"
+    dateFormat="M dd, yy"
+    @date-select="onDateSelect"
+  />
 </template>
 
 <script setup>
 defineProps({ date: Date });
+
+const emit = defineEmits(['on-update']);
+
+function onDateSelect(newValue) {
+  emit('on-update', newValue);
+}
 </script>
 
 <style scoped></style>
