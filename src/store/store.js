9f74b4830b7704846f359bbb7bf7974630566787
Mike Lee <mikemunkyu.lee@gmail.com>
2022-08-04 22:10:22 -0400

    Save user entered data as new entry

--- src/store/store.js
+++ src/store/store.js
@@ -18,7 +18,9 @@ export const useHistoryStore = defineStore('history', {
 
       this.history = formattedHistory;
     },
-    async saveEntry(title) {
+    async saveEntry(entry) {
+      const { date, amount, source, title, category, subCategory } = entry;
+
       const response = await fetch(`http://localhost:8000/users/${0}/items`, {
         method: 'POST',
         headers: {
@@ -26,16 +28,20 @@ export const useHistoryStore = defineStore('history', {
           'Content-Type': 'application/json',
         },
         body: JSON.stringify({
-          source: 'Uber',
+          source,
           title,
-          epoch_date: Date.now(),
-          amount: 1679,
-          category: 'income',
-          sub_category: 'something',
+          epoch_date: date.valueOf(),
+          amount: amount * 100, // convert to cents
+          category: category,
+          sub_category: subCategory,
         }),
       });
       const data = await response.json();
-      this.history.push(data);
+
+      this.history.push({
+        ...data,
+        date: new Date(data.epoch_date),
+      });
     },
   },
 });
