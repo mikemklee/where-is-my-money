90ba083d68791bf435ba6110fb57e427a2653657
Mike Lee <mikemunkyu.lee@gmail.com>
2022-08-04 21:29:27 -0400

    Persist input value for Amount field

--- src/components/EntryEditor/Fields/AmountField.vue
+++ src/components/EntryEditor/Fields/AmountField.vue
@@ -2,10 +2,11 @@
   <label for="currency-us">Amount</label>
   <InputNumber
     id="currency-us"
-    :value="amount"
+    :modelValue="amount"
     mode="currency"
     currency="USD"
     locale="en-US"
+    @update:modelValue="onUpdate"
   />
 </template>
 
@@ -13,6 +14,12 @@
 defineProps({
   amount: Number,
 });
+
+const emit = defineEmits(['on-update']);
+
+function onUpdate(newValue) {
+  emit('on-update', newValue);
+}
 </script>
 
 <style scoped></style>
