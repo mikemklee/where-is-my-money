a104cbf496d72a5862880cf4620e4dd35b8223fc
Mike Lee <mikemunkyu.lee@gmail.com>
2022-08-05 20:56:17 -0400

    Expose supabase instance

--- src/supabase.js
+++ src/supabase.js
@@ -2,4 +2,4 @@ import { createClient } from '@supabase/supabase-js';
 
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
 const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
-const supabase = createClient(supabaseUrl, supabaseKey);
+export const supabase = createClient(supabaseUrl, supabaseKey);
