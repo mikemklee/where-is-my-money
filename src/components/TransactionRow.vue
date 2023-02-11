88f13d1745db4fbaac416bbd54066c08ed31de1b
Mike Lee <mikemunkyu.lee@gmail.com>
2023-02-11 01:02:52 -0500

    Format transaction data for display

--- src/components/TransactionRow.vue
+++ src/components/TransactionRow.vue
@@ -1,17 +1,19 @@
 <template>
   <tr>
     <td>{{ transactionDate }}</td>
-    <td>{{ transaction.source }}</td>
+    <td>{{ transactionSource }}</td>
     <td>{{ transaction.description }}</td>
-    <td>{{ transaction.amount }}</td>
+    <td>{{ transactionAmount }}</td>
     <td>{{ transactionCategory }}</td>
-    <td>{{ transaction.account }}</td>
+    <td>{{ transactionAccount }}</td>
   </tr>
 </template>
 
 <script>
 import { format } from "date-fns";
 import { useCategoriesStore } from "@/stores/categories";
+import { useSourcesStore } from "@/stores/sources";
+import { useAccountsStore } from "@/stores/accounts";
 
 export default {
   props: {
@@ -22,17 +24,29 @@ export default {
   },
   setup(props) {
     const { categories } = useCategoriesStore();
+    const { sources } = useSourcesStore();
+    const { accounts } = useAccountsStore();
 
     const transactionDate = format(
       new Date(props.transaction.created_at),
       "MM/dd/yyyy"
     );
 
+    const transactionAmount = (props.transaction.amount / 100).toLocaleString(
+      "en-US",
+      { style: "currency", currency: "USD" }
+    );
+
     const transactionCategory = categories[props.transaction.category].name;
+    const transactionSource = sources[props.transaction.source].name;
+    const transactionAccount = accounts[props.transaction.account].name;
 
     return {
       transactionDate,
+      transactionSource,
+      transactionAmount,
       transactionCategory,
+      transactionAccount,
     };
   },
 };
