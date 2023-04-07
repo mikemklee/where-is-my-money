42f9e585f2c973029b5cda31c96adaa4772dbabe
Mike Lee <mikemunkyu.lee@gmail.com>
2023-04-07 19:12:50 -0400

    Add SelectInput component

---
+++ src/components/SelectInput.vue
@@ -0,0 +1,59 @@
+<template>
+  <div class="flex flex-col">
+    <label v-if="label" :for="id" class="text-sm">{{ label }}</label>
+    <select
+      :id="id"
+      class="border border-stone-200 py-[4px] px-2"
+      :value="value"
+      @change="handleChange"
+      :disabled="disabled"
+    >
+      <option
+        v-for="(option, index) in options"
+        :key="index"
+        :value="option.value"
+      >
+        {{ option.text }}
+      </option>
+    </select>
+  </div>
+</template>
+
+<script lang="ts">
+export default {
+  name: "SelectInput",
+  emits: ["update"],
+  props: {
+    id: {
+      type: String,
+      default: "",
+    },
+    label: {
+      type: String,
+      default: "",
+    },
+    value: {
+      type: String,
+      default: "",
+    },
+    options: {
+      type: Array,
+      required: true,
+    },
+    disabled: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  setup(props, { emit }) {
+    function handleChange(event: Event) {
+      const target = event.target as HTMLSelectElement;
+      emit("update", target.value);
+    }
+
+    return {
+      handleChange,
+    };
+  },
+};
+</script>
