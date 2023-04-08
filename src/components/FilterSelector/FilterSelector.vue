365d596f8f97db01a0dd422f2b02e8767dcd2e93
Mike Lee <mikemunkyu.lee@gmail.com>
2023-04-08 11:33:23 -0400

    Add new Tag component

--- src/components/FilterSelector/FilterSelector.vue
+++ src/components/FilterSelector/FilterSelector.vue
@@ -3,51 +3,39 @@
     <div v-if="categoriesLoaded">
       <label class="font-semibold text-sm">Categories</label>
       <div class="flex flex-wrap">
-        <div
+        <Tag
           v-for="category in categories"
           :key="category.id"
-          @click="onSelectCategoryFilter(category.id)"
-          class="whitespace-nowrap border border-gray-100 rounded px-4 py-1 text-sm mr-2 mb-2 bg-gradient-to-tr from-gray-100 cursor-pointer hover:from-gray-200 transition-all"
-          :class="{
-            'border-gray-500': categoryFilters.has(category.id),
-          }"
-        >
-          {{ category.name }}
-        </div>
+          :label="category.name"
+          :selected="categoryFilters.has(category.id)"
+          @select="onSelectCategoryFilter(category.id)"
+        />
       </div>
     </div>
 
     <div v-if="sourcesLoaded" class="mt-4">
       <label class="font-semibold text-sm">Sources</label>
       <div class="flex flex-wrap">
-        <div
+        <Tag
           v-for="source in sources"
           :key="source.id"
-          @click="onSelectSourceFilter(source.id)"
-          class="border border-gray-100 rounded px-4 py-1 text-sm mr-2 mb-2 bg-gradient-to-tr from-gray-100 cursor-pointer hover:from-gray-200 transition-all"
-          :class="{
-            'border-gray-500': sourceFilters.has(source.id),
-          }"
-        >
-          {{ source.name }}
-        </div>
+          :label="source.name"
+          :selected="sourceFilters.has(source.id)"
+          @select="onSelectSourceFilter(source.id)"
+        />
       </div>
     </div>
 
     <div v-if="accountsLoaded" class="mt-4">
       <label class="font-semibold text-sm">Accounts</label>
       <div class="flex flex-wrap">
-        <div
+        <Tag
           v-for="account in accounts"
           :key="account.id"
-          @click="onSelectAccountFilter(account.id)"
-          class="whitespace-nowrap border border-gray-100 rounded px-4 py-1 text-sm mr-2 mb-2 bg-gradient-to-tr from-gray-100 cursor-pointer hover:from-gray-200 transition-all"
-          :class="{
-            'border-gray-500': accountFilters.has(account.id),
-          }"
-        >
-          {{ account.name }}
-        </div>
+          :label="account.name"
+          :selected="accountFilters.has(account.id)"
+          @select="onSelectAccountFilter(account.id)"
+        />
       </div>
     </div>
   </div>
@@ -60,8 +48,13 @@ import { useSourcesStore } from "@/stores/sources";
 import { useAccountsStore } from "@/stores/accounts";
 import { useTransactionsStore } from "@/stores/transactions";
 
+import Tag from "@/components/FilterSelector/Tag.vue";
+
 export default {
   name: "FilterSelector",
+  components: {
+    Tag,
+  },
   setup() {
     const { categories } = useCategoriesStore();
     const { sources } = useSourcesStore();
---
+++ src/components/FilterSelector/Tag.vue
@@ -0,0 +1,35 @@
+<template>
+  <div
+    class="whitespace-nowrap border border-gray-100 rounded px-4 py-1 text-sm mr-2 mb-2 bg-gradient-to-tr from-gray-100 cursor-pointer hover:from-gray-200 transition-all"
+    :class="{ 'border-gray-500': selected }"
+    @click="onSelect"
+  >
+    {{ label }}
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "Tag",
+  props: {
+    label: {
+      type: String,
+      required: true,
+    },
+    selected: {
+      type: Boolean,
+      required: true,
+    },
+  },
+  emits: ["select"],
+  methods: {
+    onSelect() {
+      this.$emit("select");
+    },
+  },
+});
+</script>
+
+<style lang="scss" scoped></style>
