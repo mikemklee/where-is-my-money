d84e137a2c7288dade23e0486f6b2399ad4a4cad
Mike Lee <mike.lee@partnerstack.com>
2023-04-08 15:44:39 -0400

    Cleanup deps

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,6 +1,4 @@
 /* eslint-env node */
-require("@rushstack/eslint-patch/modern-module-resolution");
-
 module.exports = {
   root: true,
   extends: [
@@ -8,12 +6,6 @@ module.exports = {
     "eslint:recommended",
     "@vue/eslint-config-typescript",
     "@vue/eslint-config-prettier",
-  ],
-  overrides: [
-    {
-      files: ["cypress/e2e/**/*.{cy,spec}.{js,ts,jsx,tsx}"],
-      extends: ["plugin:cypress/recommended"],
-    },
   ],
   parserOptions: {
     ecmaVersion: "latest",
