aca6c0f45841a01dacbe9c1ba11a744b0749e9e0
Mike Lee <mikemunkyu.lee@gmail.com>
2023-04-08 23:51:56 -0400

    Add FilterGroup component

---
+++ src/components/FilterSelector/FilterGroup.vue
@@ -0,0 +1,96 @@
+<template>
+  <div>
+    <div class="flex items-center mb-2">
+      <label class="font-semibold text-sm mr-2">{{ title }}</label>
+      <div
+        @click="toggleTagEditor"
+        class="opacity-50 hover:opacity-100 transition-opacity cursor-pointer"
+      >
+        <v-icon name="pr-cog" />
+      </div>
+    </div>
+    <div
+      v-if="state.shouldShowEditor"
+      class="border-l-2 border-gray-200 my-4 p-3 flex items-center"
+    >
+      <TextInput
+        :value="state.name"
+        @update="handleUpdateName"
+        placeholder="New filter"
+        class="grow mr-2"
+      />
+      <TextButton size="sm" @click="handleSaveTag"> Save </TextButton>
+    </div>
+    <div class="flex flex-wrap">
+      <Tag
+        v-for="filter in filters"
+        :key="filter.id"
+        :label="filter.name"
+        :selected="selectedFilters.has(filter.id)"
+        @select="onSelectFilter(filter.id)"
+      />
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { reactive, defineComponent } from "vue";
+import Tag from "@/components/FilterSelector/Tag.vue";
+import TextInput from "@/components/TextInput.vue";
+import TextButton from "@/components/TextButton.vue";
+
+export default defineComponent({
+  name: "FilterGroup",
+  components: {
+    Tag,
+    TextInput,
+    TextButton,
+  },
+  props: {
+    title: {
+      type: String,
+      required: true,
+    },
+    filters: {
+      type: Object,
+      required: true,
+    },
+    selectedFilters: {
+      type: Set,
+      required: true,
+    },
+  },
+  setup(props, { emit }) {
+    function toggleTagEditor() {
+      tagEditorState.shouldShowEditor = !tagEditorState.shouldShowEditor;
+    }
+
+    const tagEditorState = reactive({
+      shouldShowEditor: false,
+      name: "",
+    });
+
+    const handleUpdateName = (value: string) => {
+      tagEditorState.name = value;
+    };
+
+    const handleSaveTag = () => {
+      console.log(tagEditorState.name);
+    };
+
+    const onSelectFilter = (filterId: number) => {
+      emit("select", filterId);
+    };
+
+    return {
+      onSelectFilter,
+      toggleTagEditor,
+      state: tagEditorState,
+      handleUpdateName,
+      handleSaveTag,
+    };
+  },
+});
+</script>
+
+<style scoped></style>
--- src/components/FilterSelector/FilterSelector.vue
+++ src/components/FilterSelector/FilterSelector.vue
@@ -1,100 +1,46 @@
 <template>
-  <div class="py-14 px-8">
-    <div v-if="categoriesLoaded">
-      <div class="flex items-center mb-2">
-        <label class="font-semibold text-sm mr-2">Categories</label>
-        <div
-          @click="toggleTagEditor('category')"
-          class="opacity-50 hover:opacity-100 transition-opacity cursor-pointer"
-        >
-          <v-icon name="pr-cog" />
-        </div>
-      </div>
-      <div
-        v-if="state.shouldShowTagEditor"
-        class="border-l-2 border-gray-200 my-4 p-3 flex items-center"
-      >
-        <TextInput
-          v-model="state.tagName"
-          placeholder="New category"
-          class="grow mr-2"
-        />
-        <TextButton size="sm" @click="handleSaveTag('category')">
-          Save
-        </TextButton>
-      </div>
-      <div class="flex flex-wrap">
-        <Tag
-          v-for="category in categories"
-          :key="category.id"
-          :label="category.name"
-          :selected="categoryFilters.has(category.id)"
-          @select="onSelectCategoryFilter(category.id)"
-        />
-      </div>
-    </div>
+  <div class="py-14 px-8 flex flex-col gap-y-6">
+    <FilterGroup
+      v-if="categoriesLoaded"
+      title="Categories"
+      :filters="categories"
+      :selectedFilters="categoryFilters"
+      @select="(id) => onSelectFilter(id, 'categories')"
+    />
 
-    <div v-if="sourcesLoaded" class="mt-4">
-      <div class="flex items-center mb-2">
-        <label class="font-semibold text-sm mr-2">Sources</label>
-        <div
-          @click="toggleTagEditor('source')"
-          class="opacity-50 hover:opacity-100 transition-opacity cursor-pointer"
-        >
-          <v-icon name="pr-cog" />
-        </div>
-      </div>
-      <div class="flex flex-wrap">
-        <Tag
-          v-for="source in sources"
-          :key="source.id"
-          :label="source.name"
-          :selected="sourceFilters.has(source.id)"
-          @select="onSelectSourceFilter(source.id)"
-        />
-      </div>
-    </div>
+    <FilterGroup
+      v-if="sourcesLoaded"
+      title="Sources"
+      :filters="sources"
+      :selectedFilters="sourceFilters"
+      @select="(id) => onSelectFilter(id, 'sources')"
+    />
 
-    <div v-if="accountsLoaded" class="mt-4">
-      <div class="flex items-center mb-2">
-        <label class="font-semibold text-sm mr-2">Accounts</label>
-        <div
-          @click="toggleTagEditor('account')"
-          class="opacity-50 hover:opacity-100 transition-opacity cursor-pointer"
-        >
-          <v-icon name="pr-cog" />
-        </div>
-      </div>
-      <div class="flex flex-wrap">
-        <Tag
-          v-for="account in accounts"
-          :key="account.id"
-          :label="account.name"
-          :selected="accountFilters.has(account.id)"
-          @select="onSelectAccountFilter(account.id)"
-        />
-      </div>
-    </div>
+    <FilterGroup
+      v-if="accountsLoaded"
+      title="Accounts"
+      :filters="accounts"
+      :selectedFilters="accountFilters"
+      @select="(id) => onSelectFilter(id, 'accounts')"
+    />
   </div>
 </template>
 
 <script lang="ts">
-import { computed, reactive, ref } from "vue";
+import { computed, reactive } from "vue";
 import { useCategoriesStore } from "@/stores/categories";
 import { useSourcesStore } from "@/stores/sources";
 import { useAccountsStore } from "@/stores/accounts";
 import { useTransactionsStore } from "@/stores/transactions";
 
-import Tag from "@/components/FilterSelector/Tag.vue";
-import TextInput from "../TextInput.vue";
-import TextButton from "../TextButton.vue";
+import FilterGroup from "./FilterGroup.vue";
+
+type FilterType = "categories" | "sources" | "accounts";
 
 export default {
   name: "FilterSelector",
   components: {
-    Tag,
-    TextInput,
-    TextButton,
+    FilterGroup,
   },
   setup() {
     const { categories } = useCategoriesStore();
@@ -102,9 +48,15 @@ export default {
     const { accounts } = useAccountsStore();
     const { getTransactions } = useTransactionsStore();
 
-    const categoryFilters = reactive(new Set());
-    const sourceFilters = reactive(new Set());
-    const accountFilters = reactive(new Set());
+    const categoryFilters = reactive(new Set<number>());
+    const sourceFilters = reactive(new Set<number>());
+    const accountFilters = reactive(new Set<number>());
+
+    const filterSets = reactive({
+      categories: categoryFilters,
+      sources: sourceFilters,
+      accounts: accountFilters,
+    });
 
     const categoriesLoaded = computed(
       () => Object.values(categories).length > 0
@@ -122,44 +74,12 @@ export default {
       await getTransactions(filters);
     };
 
-    function toggleTagEditor(tagType: string) {
-      tagEditorState.shouldShowTagEditor = !tagEditorState.shouldShowTagEditor;
-      tagEditorState.tagType = tagType;
-    }
-
-    const tagEditorState = reactive({
-      shouldShowTagEditor: false,
-      tagType: "category",
-      tagName: "",
-    });
-
-    const handleSaveTag = (tagType: string) => {
-      console.log(tagType, tagEditorState.tagName);
-    };
-
-    const onSelectCategoryFilter = (categoryId: number) => {
-      if (categoryFilters.has(categoryId)) {
-        categoryFilters.delete(categoryId);
+    const onSelectFilter = (filterId: number, filterType: FilterType) => {
+      const filterSet: Set<number> = filterSets[filterType];
+      if (filterSet.has(filterId)) {
+        filterSet.delete(filterId);
       } else {
-        categoryFilters.add(categoryId);
-      }
-      handleFilterSelection();
-    };
-
-    const onSelectSourceFilter = (sourceId: number) => {
-      if (sourceFilters.has(sourceId)) {
-        sourceFilters.delete(sourceId);
-      } else {
-        sourceFilters.add(sourceId);
-      }
-      handleFilterSelection();
-    };
-
-    const onSelectAccountFilter = (accountId: number) => {
-      if (accountFilters.has(accountId)) {
-        accountFilters.delete(accountId);
-      } else {
-        accountFilters.add(accountId);
+        filterSet.add(filterId);
       }
       handleFilterSelection();
     };
@@ -172,14 +92,9 @@ export default {
       accounts,
       accountsLoaded,
       categoryFilters,
-      onSelectCategoryFilter,
       sourceFilters,
-      onSelectSourceFilter,
       accountFilters,
-      onSelectAccountFilter,
-      toggleTagEditor,
-      state: tagEditorState,
-      handleSaveTag,
+      onSelectFilter,
     };
   },
 };
